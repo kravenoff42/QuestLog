93e4e8533a463e92e8958683b88f72bcb2087bff
Jonathan Craven <jcraven42@gmail.com>
2016-09-23 13:11:30 -0400

    Update countries.php

--- web/countries.php
+++ web/countries.php
@@ -3,14 +3,14 @@ require_once('conn.php');
 
 $cityName = "Barcelona";
 
-function getCountryByCity($db, $cityName)
+function getCountryByCity($db)
 {
   try
   {
     $stmt = $db->prepare("SELECT *  FROM city");
 
     //$stmt = $db->prepare("SELECT country.Name AS 'cntName', Code AS 'cntCode', District FROM country JOIN city ON CountryCode = Code WHERE city.Name = :cityName");
-    $stmt->bindParam(':cityName', $cityName, PDO::PARAM_STR);
+    //$stmt->bindParam(':cityName', $cityName, PDO::PARAM_STR);
     $stmt->execute();
     $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
