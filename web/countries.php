ae929e21e4fb2a8b0ed30eaefa6288c1f13f3eb8
Jonathan Craven <jcraven42@gmail.com>
2016-09-23 13:08:13 -0400

    Create countries.php

---
+++ web/countries.php
@@ -0,0 +1,34 @@
+<?php
+require_once('conn.php');
+
+$cityName = "Barcelona";
+
+function getCountryByCity($db, $cityName)
+{
+  try
+  {
+    $stmt = $db->prepare("SELECT *  FROM city");
+
+    //$stmt = $db->prepare("SELECT country.Name AS 'cntName', Code AS 'cntCode', District FROM country JOIN city ON CountryCode = Code WHERE city.Name = :cityName");
+    $stmt->bindParam(':cityName', $cityName, PDO::PARAM_STR);
+    $stmt->execute();
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+    return $results;
+
+  } catch (PDOException $e)
+  {
+    die("could not find any country with $cityName as a city.");
+  }
+
+}
+$counrties = getCountryByCity($db, $cityName);
+
+while ($counrty = $counrties->fetch())
+{
+	echo "Search Results: <br/>";
+  echo $country['Name'];
+	//echo $country['cntName'].", ".$country['cntCode'].", ".$country['District']."<br/>";
+}
+
+?>
