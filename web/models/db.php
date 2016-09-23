9e37a1b2b12982eb1ebebbfe5cd914f07e10dfea
Jonathan Craven <jcraven42@gmail.com>
2016-09-23 00:12:20 -0400

    updated DB credentials

--- web/models/db.php
+++ web/models/db.php
@@ -1,7 +1,7 @@
 <?php
-$username ="sitesender";
-$password = "IcnCcnoDarnisg1";
-$dsn = "mysql:host=localhost;dbname=questlog";
+$username ="bpbpgypkfsjbte";
+$password = "t1ygXm4Sd34IfC8e9R450w6jWV";
+$dsn = "postgres:host=ec2-23-23-226-41.compute-1.amazonaws.com;dbname=d60umun7c5mdfn";
 try
 {
   $db = new PDO($dsn, $username, $password);
