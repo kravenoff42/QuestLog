237392c9a2528b8f9be999eae741b8fda00cfe36
Jonathan Craven <jcraven42@gmail.com>
2016-09-24 10:00:53 -0400

    added while loop

--- web/countries.php
+++ web/countries.php
@@ -3,6 +3,8 @@ require_once('conn.php');
 
 $cityName = "Barcelona";
 
+  try
+  {
     $stmt = $db->prepare("SELECT * FROM city");
 
     //$stmt = $db->prepare("SELECT country.Name AS 'cntName', Code AS 'cntCode', District FROM country JOIN city ON CountryCode = Code WHERE city.Name = :cityName");
@@ -11,3 +13,19 @@ $cityName = "Barcelona";
     $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
 echo $results;
+
+  } catch (PDOException $e)
+  {
+    die("could not find any country with $cityName as a city.");
+  }
+
+
+
+while ($counrty = $results->fetch())
+{
+//	echo "Search Results: <br/>";
+  echo $country['Name'];
+//	//echo $country['cntName'].", ".$country['cntCode'].", ".$country['District']."<br/>";
+}
+
+?>
